54e8b8424072fd694085555f6389125a86541e93
furbler <63544661+furbler@users.noreply.github.com>
2023-08-15 18:19:38 +0900

    Chapter 1.3.4
    TCPクライアントでWebサーバを叩く

--- chap01/TcpClient.py
+++ chap01/TcpClient.py
@@ -4,7 +4,7 @@ def main():
     # アドレスファミリーにIPv4プロトコル、ソケットタイプにTCPソケット通信を指定
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # 指定のアドレス、ポート番号に接続要求
-    client.connect(('localhost', 8001))
+    client.connect(('localhost', 80))
 
     # client_send.txtをバイナリファイルとして読み込み、client_recv.txtにバイナリデータを新規に書き込む
     with open("client_send.txt", "rb") as send_file, open("client_recv.txt", "wb") as recv_file:
@@ -16,8 +16,8 @@ def main():
             client.send(data)
         
         # 終了を示すため、ゼロを送信
-        client.send(b'\x00')
-        print("送信完了の0を送信した。")
+        # client.send(b'\x00')
+        # print("送信完了の0を送信した。")
         
         # サーバからの返信をclient_recv.txtに出力
         while True:
